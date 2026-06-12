b13dba0acf93ee74607ad4ac303b34897f92579f
ShubhamD197 <sdhumal197@gmail.com>
2026-06-12 23:41:19 +0530

    Exercise 1: Implementing the Singleton Pattern

---
+++ Week1/Design_Patterns_and_Principles/Logger.java
@@ -0,0 +1,21 @@
+package Week1.Design_Patterns_and_Principles;
+
+public class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+        // Private constructor prevents object creation from outside
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG- " + message);
+    }
+}
