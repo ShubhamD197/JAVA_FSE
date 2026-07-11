1356989895b0c77cc6e2b72237184c3f53b1c059
ShubhamD197 <sdhumal197@gmail.com>
2026-07-12 00:01:26 +0530

    complete Hands-On 2 - data binding, lifecycle hooks, and component communication

--- week5/Angular-HandsOn/Shubham/student-course-portal/src/app/pages/course-list/course-list.ts
+++ week5/Angular-HandsOn/Shubham/student-course-portal/src/app/pages/course-list/course-list.ts
@@ -1,9 +1,54 @@
 import { Component } from '@angular/core';
+import { CourseCard } from "../../components/course-card/course-card";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [CourseCard,CommonModule],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {}
+
+export class CourseList {
+  
+  courses = [
+  {
+    id: 1,
+    name: 'Angular',
+    code: 'ANG101',
+    credits: 4
+  },
+  {
+    id: 2,
+    name: 'Java',
+    code: 'JAVA201',
+    credits: 3
+  },
+  {
+    id: 3,
+    name: 'Spring Boot',
+    code: 'SPR301',
+    credits: 4
+  },
+  {
+    id: 4,
+    name: 'Database Systems',
+    code: 'DB401',
+    credits: 3
+  },
+  {
+    id: 5,
+    name: 'Cloud Computing',
+    code: 'CC501',
+    credits: 4
+  }
+  ];
+
+selectedCourseId?: number;
+
+onEnroll(courseId: number) {
+  console.log('Enrolling in course:', courseId);
+
+  this.selectedCourseId = courseId;
+}
+}
--- week5/Angular-HandsOn/Shubham/student-course-portal/src/app/pages/home/home.ts
+++ week5/Angular-HandsOn/Shubham/student-course-portal/src/app/pages/home/home.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+
+export class Home implements OnInit, OnDestroy{
+  portalName = 'Student Course Portal';
+
+  // [property] performs one-way binding from the component to the view,
+  isPortalActive = true;
+
+  message = '';
+
+  onEnrollClick(){
+    this.message = "Enrollment opened";
+  }
+
+  // while [(ngModel)] provides two-way binding between the component and the view.
+  searchTerm = "";
+
+  availableCourses = 0;
+
+  ngOnInit(): void {
+    this.availableCourses = 12;
+
+    console.log(`Home component initialised: Total courses loaded ` + this.availableCourses);
+  }
+
+  ngOnDestroy(): void {
+  console.log('HomeComponent destroyed');
+}
+}
+
+
+
